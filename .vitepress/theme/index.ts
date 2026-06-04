3c0f4a7e093f909f9db80502b085c1906e119a1e
areskill <areskul@areskul.com>
2026-06-04 23:56:39 +0200

    ci: deps update to latest

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,10 @@
 import { App, h } from "vue";
 import DefaultTheme from "vitepress/theme";
 import "./css/index.css";
+
 // Components
-import PreferenceSwitch from "@components/PreferenceSwitch.vue";
-import VPSwitch from "vitepress/dist/client/theme-default/components/VPSwitch.vue";
+// import PreferenceSwitch from "@components/PreferenceSwitch.vue";
+// import VPSwitch from "vitepress/dist/client/theme-default/components/VPSwitch.vue";
 
 export default {
   extends: DefaultTheme
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   build: {
     sourcemap: false
   },
+  plugins: [tailwindcss()],
   resolve: {
     alias: {
       "@": "/src",
